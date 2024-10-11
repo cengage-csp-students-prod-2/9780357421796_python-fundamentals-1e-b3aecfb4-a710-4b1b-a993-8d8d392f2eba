34178fc3141cf9c628463e127bbfa951a80208f5
beccaminahanctstate <rebecca.minahanrowley@ctstate.edu>
2024-10-11 18:22:37 +0000

    Testing Fri Oct 11 2024 18:22:37 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex03A/student/main.py
+++ chapter1/ex03A/student/main.py
@@ -3,7 +3,7 @@ Time = 3
 Knots = Distance/1.15078
 Feet = 60*5280 
 Seconds = 3*3600
-MPH = Distance/Time
+MPH = Distance/Time 
 FPS = Feet/Seconds
 print("The speed in knots is:", Knots)
 print("The speed in miles per hour is", MPH)
