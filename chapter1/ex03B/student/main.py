05feaa844ec8b82cb0da73dd8e1f5defd60adf6c
beccaminahanctstate <rebecca.minahanrowley@ctstate.edu>
2024-10-11 17:44:16 +0000

    Testing Fri Oct 11 2024 17:44:16 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex03B/student/main.py
+++ chapter1/ex03B/student/main.py
@@ -1,1 +1,6 @@
-# Write your code here
+PI = 3.14159
+radius = 3
+diameter = 2 * radius
+area = PI * (radius ** 2)
+print("The diameter of the circle is:", diameter)
+print ("The area of the circle is:", area)
