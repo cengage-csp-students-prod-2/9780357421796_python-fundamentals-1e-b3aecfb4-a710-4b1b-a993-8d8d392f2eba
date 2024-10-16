cdc95c5be59823d03e56d53e3d0da0721da3b050
beccaminahanctstate <rebecca.minahanrowley@ctstate.edu>
2024-10-16 19:29:30 +0000

    Testing Wed Oct 16 2024 19:29:30 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex08/student/main.py
+++ chapter3/ex08/student/main.py
@@ -1,1 +1,5 @@
-# Write your code here
+calc = 0 
+for even in range (6, 16,2):
+    for odd in range (5, 16, 2):
+        calc = even + odd
+        print(f"{even} + {odd} = {calc}")
--- chapter3/ex09/student/main.py
+++ chapter3/ex09/student/main.py
@@ -1,1 +1,7 @@
-# Write your code here
+for value in range (10,101):
+    if value % 5 != 0:
+        continue
+    if value == 95:
+        break 
+    print(value)
+    pass
