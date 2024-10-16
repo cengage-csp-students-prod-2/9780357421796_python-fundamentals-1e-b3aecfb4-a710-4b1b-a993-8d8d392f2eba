d314bfd0281ed781687d5dd7318f8bdf58b041fa
beccaminahanctstate <rebecca.minahanrowley@ctstate.edu>
2024-10-16 16:01:03 +0000

    Testing Wed Oct 16 2024 16:01:02 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex03/student/main.py
+++ chapter3/ex03/student/main.py
@@ -1,1 +1,10 @@
-# Write your code here
+name = input("Enter your name:")
+password = "Pas$Word"
+first = True
+while True: 
+    user_input = input("Enter your password:")
+    if user_input == password:
+        print("Wecome back", name)
+        break
+    else:
+        print("Incorrect password, try again...")
