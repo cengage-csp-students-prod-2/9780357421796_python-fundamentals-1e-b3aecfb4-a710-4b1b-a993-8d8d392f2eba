5115b71acacba9234d04686eb909a05d193e3a95
beccaminahanctstate <rebecca.minahanrowley@ctstate.edu>
2024-10-16 19:41:54 +0000

    Testing Wed Oct 16 2024 19:41:54 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/main.py
+++ chapter4/ex03/student/main.py
@@ -1,1 +1,6 @@
-# Write your code here
+skip_integers(*args):
+    for value in args:
+        if isinstance (value,int):
+            continue
+        print(value)
+skip_integers(5.2, 42, 'value', 6.0, 100, True, None)
