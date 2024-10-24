7a32998af6010cf874219b92fc8e480cbc04f940
beccaminahanctstate <rebecca.minahanrowley@ctstate.edu>
2024-10-24 23:55:30 +0000

    Testing Thu Oct 24 2024 23:55:30 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/main.py
+++ chapter5/ex06/student/main.py
@@ -1,1 +1,8 @@
-# Write your code here
+cars = ('coupe','coupe', 'coupe', 'carbiolet','sedan')
+fav = cars.count('coupe')
+amt = len(cars)
+percentage_coup = (fav/amt) * 100
+if percentage_coup > 45:
+    print("Too many coupes in the garage.")
+else:
+    print ("You are all set with cars.")
